bca7332f167e3045ff4254222f45b6accdd4d8e7
G2A0U <159654720+G2A0U@users.noreply.github.com>
2024-09-26 09:21:28 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,22 +4,21 @@ const unmuteNotice = document.getElementById('unmuteNotice');
 const light = document.querySelector('.light');
 const overlay = document.querySelector('.overlay');
 
-// Fonction pour démarrer la vidéo et activer le son après un clic
+
 const startVideoWithSound = () => {
   video.play(); // Démarre la vidéo
   video.muted = false; // Active le son
   unmuteNotice.style.display = 'none'; // Cache le message
 };
 
-// Ajoute un écouteur d'événement pour démarrer la vidéo lors d'un clic sur le document
+
 document.addEventListener('click', startVideoWithSound);
 
-// Gestion de l'effet de lumière qui suit la souris
+
 document.body.addEventListener('mousemove', (event) => {
   const x = event.clientX;
   const y = event.clientY;
 
-  // Met à jour les variables CSS pour la lumière et l'overlay
   light.style.setProperty('--x', `${x}px`);
   light.style.setProperty('--y', `${y}px`);
   overlay.style.setProperty('--x', `${x}px`);
