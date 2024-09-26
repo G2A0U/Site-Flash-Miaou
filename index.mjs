c59083b1a3ac307f0fb08db9da90cefcc35914fb
G2A0U <159654720+G2A0U@users.noreply.github.com>
2024-09-26 10:40:45 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,16 +1,26 @@
-const videoMask = document.getElementById('videoMask');
+const video = document.getElementById('myVideo');
+const unmuteNotice = document.getElementById('unmuteNotice');
+const light = document.querySelector('.light');
+const overlay = document.querySelector('.overlay');
 
-// Met à jour la position du cercle de révélation autour de la souris
-document.querySelector('.video-container').addEventListener('mousemove', (event) => {
-  const rect = event.target.getBoundingClientRect();
-  const x = event.clientX - rect.left; // Position relative dans le conteneur vidéo
-  const y = event.clientY - rect.top;
 
-  // Ajuste le clip-path pour créer un effet de cercle autour du curseur
-  videoMask.style.clipPath = `circle(10% at ${x}px ${y}px)`;
+const startVideoWithSound = () => {
+  video.play(); // Démarre la vidéo
+  video.muted = false; // Active le son
+  unmuteNotice.style.display = 'none'; // Cache le message
+};
+
+
+document.addEventListener('click', startVideoWithSound);
+
+
+document.body.addEventListener('mousemove', (event) => {
+  const x = event.clientX;
+  const y = event.clientY;
+
+  light.style.setProperty('--x', `${x}px`);
+  light.style.setProperty('--y', `${y}px`);
+  overlay.style.setProperty('--x', `${x}px`);
+  overlay.style.setProperty('--y', `${y}px`);
 });
 
-// Si la souris quitte la zone, cache de nouveau complètement la vidéo
-document.querySelector('.video-container').addEventListener('mouseleave', () => {
-  videoMask.style.clipPath = 'circle(0% at 50% 50%)'; // Cache la vidéo en dehors du survol
-});
