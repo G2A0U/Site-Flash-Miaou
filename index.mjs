efe0e9035e32ffd4f63e0a653c2de4c5ac8d4b68
G2A0U <159654720+G2A0U@users.noreply.github.com>
2024-09-26 10:35:46 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,8 +2,9 @@ const videoMask = document.getElementById('videoMask');
 
 // Met à jour la position du cercle de révélation autour de la souris
 document.querySelector('.video-container').addEventListener('mousemove', (event) => {
-  const x = event.offsetX;
-  const y = event.offsetY;
+  const rect = event.target.getBoundingClientRect();
+  const x = event.clientX - rect.left; // Position relative dans le conteneur vidéo
+  const y = event.clientY - rect.top;
 
   // Ajuste le clip-path pour créer un effet de cercle autour du curseur
   videoMask.style.clipPath = `circle(10% at ${x}px ${y}px)`;
@@ -11,5 +12,5 @@ document.querySelector('.video-container').addEventListener('mousemove', (event)
 
 // Si la souris quitte la zone, cache de nouveau complètement la vidéo
 document.querySelector('.video-container').addEventListener('mouseleave', () => {
-  videoMask.style.clipPath = 'circle(0% at 0 0)'; // Cache la vidéo en dehors du survol
+  videoMask.style.clipPath = 'circle(0% at 50% 50%)'; // Cache la vidéo en dehors du survol
 });
